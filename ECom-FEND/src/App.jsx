7b4bf76f3208cee041b9981fce2cb87196fc9727
ArsiyaTasleem90250 <2400090250@kluniversity.in>
2025-10-31 18:07:29 +0530

    Done!

--- ECom-FEND/src/App.jsx
+++ ECom-FEND/src/App.jsx
@@ -1,16 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
-import {BrowserRouter as Router,Routes,Route} from "react-router-dom";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
+import AboutUs from "./components/AboutUs";
+import Dashboard from "./components/Dashboard";
+import Products from "./components/products";
 
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/aboutus" element={<AboutUs />} />
+        <Route path="/dashboard" element={<Dashboard/>} />
+        <Route path="/products" element={<Products/>} />
 
-
-function App()
-{
-    return(
-      <Router>
-        <Routes>
-          <Route path = "/" element={<HomePage/>}></Route>
-        </Routes>
-      </Router>
-    )
+       
+      </Routes>
+    </Router>
+  );
 }
 export default App;
